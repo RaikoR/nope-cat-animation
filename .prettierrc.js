afc6d82ff8849f24fff2be0fd7dd8f09882397ca
rraidma <raiko@play.ee>
2020-11-24 12:29:16 +0200

    Fixed code linters

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,10 @@
 module.exports = {
-  semi: true,
-  singleQuote: true,
-  tabWidth: 2,
-  bracketSpacing: true,
-  trailingComma: 'none',
-  printWidth: 120,
-  endOfLine: 'auto'
+    semi: true,
+    singleQuote: true,
+    tabWidth: 4,
+    bracketSpacing: true,
+    trailingComma: 'none',
+    printWidth: 120,
+    endOfLine: 'auto',
+    useTabs: false,
 };
